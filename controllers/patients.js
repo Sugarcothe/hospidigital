fced491630d4a48587841bf3caed23f3ec92b60b
Suagrcothe <ifeanyivalentine82@gmail.com>
2023-01-30 17:52:35 +0100

    update patients

--- controllers/patients.js
+++ controllers/patients.js
@@ -16,12 +16,26 @@ export const register = async (req, res, next) => {
 export const login = async (req, res, next) => {
   try {
     const patient = await usersPatientsModel.findOne({
-      hospitalID: req.body.hospitalID
+      hospitalID: req.body.hospitalID,
     });
     await res.status(200).json({
       "Logged in succesfuly into patients dashboard": patient,
     });
   } catch (err) {
     next(err);
+  }
+};
+
+export const update = async (req, res, next) => {
+  try {
+    const updatedPatient = await usersPatientsModel.findByIdAndUpdate(
+      req.params.id,
+      { $set: req.body },
+      { new: true }
+    );
+    await res.status(200).json(updatedPatient);
+  } catch (err) {
+    await res.status(400).json(err);
+    console.log(err)
   }
 };
--- routes/patients.js
+++ routes/patients.js
@@ -1,8 +1,8 @@
-import express from "express"
-import { register, login } from "../controllers/patients.js";
-
+import express from "express";
+import { register, login, update } from "../controllers/patients.js";
 
 export const patientsRouter = express.Router();
 
-patientsRouter.post("/registerPatients", register)
+patientsRouter.post("/registerPatients", register);
 patientsRouter.post("/loginPatients", login);
+patientsRouter.put("/updatePatients/:id", update);
